883d5e3da13627f0add02c50632b0a76fc667c13
Cairo <cairo.92@hotmail.com>
2026-01-22 21:18:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import axios from "axios";
 import pg from "pg";
 
 dotenv.config();
+
+console.log("DATABASE_URL =>", process.env.DATABASE_URL);
+
 const app = express();
 const port = process.env.PORT || 3000;
 
